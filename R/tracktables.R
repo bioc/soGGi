1614c471c55defae6780820afef644205c693fad
Tom Carroll <tc.infomatics@gmail.com>
2015-10-28 15:57:27 +0000

    fix for readGAlingmentsFromBam to readGAlignments
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/soGGi@110034 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tracktables.R
+++ R/tracktables.R
@@ -234,7 +234,7 @@ runRegionPlot <- function(bamFile,testRanges,samplename=NULL,nOfWindows=100,Frag
   ##  Calculate fragment length from cross-coverage if not provided
   
     if(paired==FALSE){
-      total <- readGAlignmentsFromBam(bamFile,param=Param)
+      total <- readGAlignments(bamFile,param=Param)
       message("..Done.\nRead in ",length(total)," reads")
       
       if(is.null(FragmentLength)){
@@ -251,7 +251,7 @@ runRegionPlot <- function(bamFile,testRanges,samplename=NULL,nOfWindows=100,Frag
   
     if(paired==TRUE){
       
-      gaPaired <- readGAlignmentsFromBam(bamFile, 
+      gaPaired <- readGAlignments(bamFile, 
                                          param=ScanBamParam(what=c("mpos"),
                                                             flag=scanBamFlag(isProperPair = TRUE,isFirstMateRead = TRUE)))      
       tempPos <- GRanges(seqnames(gaPaired[strand(gaPaired) == "+"]),
